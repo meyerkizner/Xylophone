e3e842cc68ae8867d6998d160eeccd517fa54271
Meyer Kizner <meyer@pre-alpha.com>
2011-11-24 14:48:31 -0500

    Add new methods and documentation to Dispatcher and DispatcherAsync for subscription registration, checking, and cancellation

--- src/main/java/com/prealpha/xylophone/shared/DispatcherAsync.java
+++ src/main/java/com/prealpha/xylophone/shared/DispatcherAsync.java
@@ -16,11 +16,17 @@
 
 package com.prealpha.xylophone.shared;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * The asynchronous interface for event dispatching. Most users of this library
  * will not need to write an implementation of this interface.
+ * <p>
+ * 
+ * All methods throw {@link NullPointerException} if any parameters are
+ * {@code null}.
  * 
  * @author Meyer Kizner
  * @see Dispatcher
@@ -36,9 +42,50 @@ public interface DispatcherAsync {
 	 *            an action to execute
 	 * @param callback
 	 *            a callback to receive the complete result of the action
-	 * @throws NullPointerException
-	 *             if either {@code action} or {@code callback} is {@code null}
 	 * @see Dispatcher#execute(Action)
 	 */
 	<R extends Result> void execute(Action<R> action, AsyncCallback<R> callback);
+
+	/**
+	 * The asynchronous version of {@link Dispatcher#subscribe(Predicate)}.
+	 * 
+	 * @param <A>
+	 *            the action type for the subscription
+	 * @param <R>
+	 *            the result type for the subscription
+	 * @param predicate
+	 *            a predicate matching actions to which a subscription is
+	 *            desired
+	 * @param callback
+	 *            a callback to receive the subscription
+	 * @see Dispatcher#subscribe(Predicate)
+	 */
+	<A extends Action<R>, R extends Result> void subscribe(
+			Predicate<? super A> predicate,
+			AsyncCallback<Subscription<R>> callback);
+
+	/**
+	 * The asynchronous version of {@link Dispatcher#check(Subscription)}.
+	 * 
+	 * @param <R>
+	 *            the result type for the subscription
+	 * @param subscription
+	 *            the subscription to check
+	 * @param callback
+	 *            a callback to receive the list of results
+	 * @see Dispatcher#check(Subscription)
+	 */
+	<R extends Result> void check(Subscription<R> subscription,
+			AsyncCallback<ImmutableList<R>> callback);
+
+	/**
+	 * The asynchronous version of {@link Dispatcher#cancel(Subscription)}.
+	 * 
+	 * @param subscription
+	 *            the subscription to cancel
+	 * @param callback
+	 *            a callback to be notified on completion
+	 * @see Dispatcher#cancel(Subscription)
+	 */
+	void cancel(Subscription<?> subscription, AsyncCallback<Void> callback);
 }
